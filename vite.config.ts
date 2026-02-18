bbe5e48cf70ec309e7ec01d5eea2966225871cab
vebjorn1997 <11070506@uvu.edu>
2026-02-17 22:23:52 -0700

    u

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
