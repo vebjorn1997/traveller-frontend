5b95423c06ea358eb9cfefb665c25ff1976be206
vebjorn1997 <11070506@uvu.edu>
2026-02-17 21:53:06 -0700

    click header to go home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import { Link } from "@tanstack/react-router"
+
 export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between p-4 border-b border-gray-700">
-        <h2 className="text-xl font-bold">Traveller Dashboard</h2>
+        <Link to="/">
+          <h2 className="text-xl font-bold">Traveller Dashboard</h2>
+        </Link>
       </header>
     </>
   )
